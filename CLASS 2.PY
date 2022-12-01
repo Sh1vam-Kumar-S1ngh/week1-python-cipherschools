11b7940ea33d4c2e947e7c34bab4f91a8ff1c99b
Sh1vam-Kumar-S1ngh <119123409+Sh1vam-Kumar-S1ngh@users.noreply.github.com>
2022-12-01 23:49:21 +0530

    Create CLASS 2.PY

---
+++ CLASS 2.PY
@@ -0,0 +1,30 @@
+print("hello world")
+
+# this is a comment 
+
+
+'''this is 
+also another form of comment'''
+
+
+# primitive datatypes
+# 1. integers
+# 2. float
+# 3. string
+# 4. complex   
+
+a = "Cipher"
+print(type(a))
+
+b = 15
+print(type(b))
+
+c = 123.56
+print(type(c))
+
+# variables:
+
+c = "Amaan"
+d = 132
+print(c)
+print(d)
