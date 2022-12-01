60ed8eeaa73f8106d107ec9fec4561cc7ec42866
Sh1vam-Kumar-S1ngh <119123409+Sh1vam-Kumar-S1ngh@users.noreply.github.com>
2022-12-01 23:52:45 +0530

    Create CLASS 5.py

---
+++ CLASS 5.py
@@ -0,0 +1,44 @@
+print(15+.34)
+print(4-1)
+print(isinstance(10.0,int))
+print(10/2)
+print(13//3) #floor division
+print(3*9)
+print(4**2) #exponentiation
+print(10%3)
+print("Cipher"+"Schools") 
+print("SHIVAM"*6)
+print("ANURAG" "" "Katyal") #string catenation
+
+#comparision operators
+print(1==2)
+print(1!=2)
+print(1<2)
+print(2>3)
+print("z" > "abc") #lexicographical comparision
+print("a"<"A")  #compared using ascii values
+
+#Logical Operators
+print(True and False)
+print(True or False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+print(3 and 6)
+print(isinstance(True,int)) #Returns True--->True is integer
+print(type(True))
+print(bool(1))
+
+#short curcuiting
+"""a or true=true
+   a or false=a
+   a and true=a
+   a and false=false"""
+   
+print("jatin" and 6)
+print("" and 6)
+print(1.6 or "")
+print("" or 2.5)
+print(bool(""))
+print(bool([]))
+print(bool([1,2,3]))
